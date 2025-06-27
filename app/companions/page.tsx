33d6dfd9cec1ea408167088feeac23c2bdc4af3e
azizjan18 <websbyaziz@gmail.com>
2025-06-27 16:04:50 +0500

    implemented authentication, billing, database setup, create companion server action

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import {getAllCompanions} from "@/lib/actions/companion.actions";
+import CompanionCard from "@/components/ui/CompanionCard";
+import {getSubjectColor} from "@/lib/utils";
+import Searchinput from "@/components/ui/Searchinput";
+import SubjectFilter from "@/components/ui/SubjectFilter";
 
-const page = () => {
+const CompanionLibrary = async ({searchParams}:SearchParams) => {
+  const filters = await searchParams
+  const subject = filters.subject ?filters.subject :'';
+  const topic = filters.topic ?filters.topic :'';
+
+  const  companions = await  getAllCompanions({subject,topic})
+
   return (
-    <div>companionLibrary</div>
+    <main>
+      <section className="flex justify-between gap-4 max-sm:flex-col">
+          <h1>companion library</h1>
+        <div className='flex gap-4'>
+          <Searchinput/>
+          <SubjectFilter/>
+        </div>
+      </section>
+      <section className="companions-grid">
+        {companions?.map((companion)=>(
+            <CompanionCard key={companion.id} {...companion}
+            color={getSubjectColor(companion.subject)}/>
+        ))}
+      </section>
+    </main>
   )
 }
 
-export default page
+export default CompanionLibrary
